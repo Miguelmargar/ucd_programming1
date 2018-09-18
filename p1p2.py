c4602b4f60b02781071e946775afbfb0fdbd6eb3
Miguel Martinez <miguelitalia@hotmail.com>
2018-09-18 12:51:50 +0000

    practical one

---
+++ p1p2.py
@@ -0,0 +1,5 @@
+# My second program
+print("Good morning!")
+print("Vietnam!")
+print("Good morning,","Vietnam!")
+
---
+++ p1p3.py
@@ -0,0 +1,7 @@
+# My third program
+print("Name: Miguel Martinez")
+print("")
+print("Address: 4 Westminster Park, Mount Brown, Blackrock, D8")
+print("")
+print("Phone no.: 0873752689")
+    
